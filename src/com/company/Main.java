e62706ba0953e904b8a66545739ad0e0c263fcee
eg913 <eg913@pvsd.org>
2018-12-13 11:20:23 -0500

    not even close

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner numberReader = new Scanner(System.in);
+        System.out.println("How many boolean expressions would you like to input?");
+        int index = numberReader.nextInt();
+
+
+        String [] operations = new String [100];
+        operations[0]= "A";
+        operations[1]= "B";
+        boolean [] tt = new boolean [operations.length];
+        boolean [] tf = new boolean [operations.length];
+        boolean [] ft = new boolean [operations.length];
+        boolean [] ff = new boolean [operations.length];
+
+        tt[0] = true; tt[1] = true;
+        tf[0] = true;
+        ft[1] = true;
+
+        for(int i = 2; i < operations.length; i++){
+            Scanner expressionReader = new Scanner(System.in);
+            System.out.println("Input Boolean Expression");
+            operations[i] = expressionReader.nextLine();
+
+
+
+        }
+
+
 
 
     }
